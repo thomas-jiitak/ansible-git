54135a4af652874f4f2ffdd904d3fb8222d61e88
thomas-jiitak <thomasjoseph@jiitak.com>
2024-09-06 13:47:32 +0530

    changes

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World! its Ansible Deployment.!!!! YaaaRRR')
+  res.send('new DAY')
 })
 
 app.listen(port, () => {
