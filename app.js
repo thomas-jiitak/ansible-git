ab9a76d97af336255dd56e577727cd06e1db03d0
thomas-jiitak <thomasjoseph@jiitak.com>
2024-09-06 12:49:16 +0530

    nabjdksn

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World! INDIA')
+  res.send('Hello World! its Ansible Deployment.!!!!')
 })
 
 app.listen(port, () => {
